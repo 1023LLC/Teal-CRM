668d266030577cf28605ef4e2d8f2baadfd4ebe5
julius <nyerereoffice@gmail.com>
2024-04-05 07:05:57 +0300

    Client Detail Page

--- client/urls.py
+++ client/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 
+app_name='client'
+
+
 urlpatterns = [
-    path('', views.clients_list, name='clients_list')
+    path('', views.clients_list, name='clients_list'),
+    path('<int:pk>/', views.clients_detail, name='clients_detail')
 ]
--- client/views.py
+++ client/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.contrib.auth.decorators import login_required
 
@@ -9,4 +9,11 @@ from .models import Client
 @login_required
 def clients_list(request):
     clients = Client.objects.filter(created_by=request.user)
+    
     return render(request, 'client/clients_list.html', {'clients':clients})
+
+@login_required
+def clients_detail(request, pk):
+    client = get_object_or_404(Client, created_by=request.user, pk=pk)
+    
+    return render(request, 'client/clients_detail.html', {'client':client})
