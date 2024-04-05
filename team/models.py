7f26a9aed421395d8d15e7fc3dbfbec63eafcda9
julius <nyerereoffice@gmail.com>
2024-04-05 14:08:15 +0300

    Order in Models

--- team/models.py
+++ team/models.py
@@ -10,5 +10,9 @@ class Team(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, )
 
 
+    class Meta:
+        ordering = ('name',)
+    
+
     def __str__(self):
         return self.name
